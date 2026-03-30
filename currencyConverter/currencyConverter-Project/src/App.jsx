1f6b1ff18061c32ce4cc0a8f08aa93d08c24025a
HemanthKumar N <hemanthnanjappaji@gmail.com>
2026-03-30 16:16:57 +0530

    Currency-Converter

---
+++ currencyConverter/currencyConverter-Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { use, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-3xl bg-orange-500 rounded' style={{textAlign:"center", marginTop:"20px", fontWeight:"700"}}>Curreny App</h1>
+    </>
+  )
+}
+
+export default App
