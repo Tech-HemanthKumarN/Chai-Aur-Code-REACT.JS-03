a7f45310f703379c3714fbb1042d28dacd568f9d
HemanthKumar N <hemanthnanjappaji@gmail.com>
2026-04-01 22:15:55 +0530

    added reactRouter project

--- reactRouter-Proj/reactRouter/src/App.jsx
+++ reactRouter-Proj/reactRouter/src/App.jsx
@@ -2,6 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+//ABOUT LINK
+
+// When ever u use  "a" tag the will refresh completly but in react we don't  have a concept of refresh so we use "Link"
+// In standard HTML, we use <a href="...">. However, in React, using an anchor tag is discouraged for internal navigation.
+
+// Anchor Tag (<a>): Refreshes the entire page, reloading all assets and losing the application state.
+
+// Link Component:
+
+// Usage: import { Link } from 'react-router-dom'
+
+// Syntax: <Link to="/about">About</Link>
+
+// Interview Tip: Link prevents page reloads by intercepting the click and updating the URL via the History API, then rendering only the necessary component.
+
+//About NavLink
+
+// 🟠 3. NavLink Component
+// NavLink is a special version of the Link that knows whether or not it is "active."
+
+// Key Feature: It provides access to an isActive state within its className or style props.
+
+// Practical Usage: Useful for highlighting the current page in a navigation bar (e.g., changing the text color to orange when on that page).
+
+// Example Code Snippet:
+
+// JavaScript
+// <NavLink
+//   to="/about"
+//   className={({ isActive }) => 
+//     `block py-2 ${isActive ? "text-orange-700" : "text-gray-700"}`
+//   }
+// >
+//   About
+// </NavLink>
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+import{Link, NavLink} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
 import './App.css'
 
 function App() {
@@ -9,111 +54,7 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+  
     </>
   )
 }
--- reactRouter-Proj/reactRouter/src/main.jsx
+++ reactRouter-Proj/reactRouter/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import "./index.css";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import About from "./components/About/About.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+
+
+    path :'/',
+    element:<Layout />,
+    children:[
+      {
+        path:"",
+        element:<Home />
+      },
+
+      {
+        path:"about",
+        element:<About />
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
